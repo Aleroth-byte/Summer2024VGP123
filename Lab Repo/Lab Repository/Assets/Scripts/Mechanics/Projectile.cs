693dea95f511434904bf8f7311fb582e3a78f798
Aleroth-byte <dfernandezc06@gmail.com>
2024-07-30 10:37:03 -0600

    Lab 4

---
+++ Lab Repo/Lab Repository/Assets/Scripts/Mechanics/Projectile.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Projectile : MonoBehaviour
+{
+    [SerializeField, Range(1,50)] public float lifetime;
+
+    [HideInInspector]
+    public float xVel;
+    [HideInInspector]
+    public float yVel;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (lifetime <= 0) lifetime = 2.0f;
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2 (xVel, yVel);
+        Destroy (gameObject, lifetime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Power"))
+        Destroy (this.gameObject);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+        Destroy(this.gameObject);
+         
+    }
+
+}
+
